d4140667ae9da5aff90c6e64433562ea6fa7d179
Leidysalda <leidysalda1@gmail.com>
2020-06-17 02:12:58 +0000

    Add file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,18 +19,15 @@ void bubble_sort(int *array, size_t size)
 	if (size == 0)
 		return;
 
-	if (array)
-	{
-		for (i = 0; i < size; i++)
-			for (j = 0; j < size - 1; j++)
+	for (i = 0; i < size; i++)
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
 			{
-				if (array[j] > array[j + 1])
-				{
-					aux = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = aux;
-					print_array(array, size);
-				}
+				aux = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = aux;
+				print_array(array, size);
 			}
-	}
+		}
 }
