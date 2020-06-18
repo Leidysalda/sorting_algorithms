e656ff1c5eba609f9dfefeee16354a3a7351081e
Leidysalda <leidysalda1@gmail.com>
2020-06-18 03:00:00 +0000

    Add file

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,57 +1,43 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include "sort.h"
 
 /**
- * quick_sort1 - function that sorts an array of integers in ascending order
- * using the Quick sort algorithm.
- *
- * @list: array
- * @left: left
- * @right: right
- *
- * Return: void
- */
-
-void quick_sort1(int list[], size_t left, size_t right)
-{
-	size_t izq = left, temp;
-	size_t der = right - 1;
-	size_t pivot = (sizeof(list) / 2);
-
-	do
-	{
-		while (list[izq] < pivot && izq < right)
-			izq++;
-		while (pivot < list[der] && der > left)
-			der--;
-		if (izq <= der)
-		{
-			temp = list[izq];
-			list[izq] = list[der];
-			list[der] = temp;
-			izq++;
-			der--;
-		}
-	}while (izq <= der);
-	if (left < der)
-		quick_sort1(list, left, der);
-	if (right < izq)
-		quick_sort1(list, izq, left);
-}
-
-/**
- * quick_sort - function that sorts an array of integers in ascending order
- * using the Quick sort algorithm.
- *
+ * quick_sort - sorts an array of integers in ascending order.
  * @array: array
  * @size: size array
  *
- * Return: void
  */
 
 void quick_sort(int *array, size_t size)
 {
-	quick_sort1(array, 0, size-1);
-	quick_sort1(array, 0 + 1, size);
+
+	int pivot, temp, i, j;
+	int len = size / 2;
+
+	if (size < 2)
+		return;
+
+
+	pivot = array[len];
+
+	for (i = 0, j = size - 1; ; i++, j--)
+	{
+		while (array[i] < pivot)
+			i++;
+		while (array[j] > pivot)
+			j--;
+
+		if (i >= j)
+			break;
+
+		temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	print_array(array, size);
+
+	quick_sort(array, i);
+	quick_sort(array + i, size - i);
+	print_array(array, size);
+
 }
